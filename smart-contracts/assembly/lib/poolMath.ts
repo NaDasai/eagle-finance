16a56eb508c022d542831c53f9543652a107be94
Ayoub Amer <ayouub.ameur@gmail.com>
2024-12-11 14:38:25 +0100

    fix: adjust poolMath functions calculation

--- smart-contracts/assembly/lib/poolMath.ts
+++ smart-contracts/assembly/lib/poolMath.ts
@@ -1,11 +1,11 @@
 import { u256 } from 'as-bignum/assembly';
 
 export function getInputAmountNet(inputAmount: u256, feeRate: u16): u256 {
-  const fee = u256.mul(
-    inputAmount,
-    u256.div(u256.fromU64(feeRate), u256.fromF64(100)),
+  // fee = (inputAmount * feeRate) / 100
+  const fee = u256.div(
+    u256.mul(inputAmount, u256.fromU64(feeRate)),
+    u256.fromU64(100),
   );
-
   const inputAmountNet = u256.sub(inputAmount, fee);
 
   return inputAmountNet;
@@ -16,6 +16,7 @@ export function getAmountOut(
   inputReserve: u256,
   outputReserve: u256,
 ): u256 {
+  // amountOut = (inputAmount * outputReserve) / (inputReserve + inputAmount)
   const f = u256.mul(outputReserve, inputAmount);
   const f2 = u256.add(inputReserve, inputAmount);
 
