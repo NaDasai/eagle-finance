023c0273fa7d06103b25f1be3f6034589e8f9227
Ayoub Amer <ayouub.ameur@gmail.com>
2024-12-12 17:22:27 +0100

    feat: script to deploy registry smart contract

---
+++ smart-contracts/src/deployRegistry.ts
@@ -0,0 +1,34 @@
+import {
+  Account,
+  Args,
+  Mas,
+  SmartContract,
+  Web3Provider,
+} from '@massalabs/massa-web3';
+import { getScByteCode } from './utils';
+
+const account = await Account.fromEnv();
+const provider = Web3Provider.buildnet(account);
+
+console.log('Deploying contract...');
+
+const byteCode = getScByteCode('build', 'registry.wasm');
+
+const constructorArgs = new Args().serialize();
+
+const contract = await SmartContract.deploy(
+  provider,
+  byteCode,
+  constructorArgs,
+  { coins: Mas.fromString('0.1') },
+);
+
+console.log('Contract deployed at:', contract.address);
+
+const events = await provider.getEvents({
+  smartContractAddress: contract.address,
+});
+
+for (const event of events) {
+  console.log('Event message:', event.data);
+}
