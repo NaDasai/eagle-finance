4a4ebfcaefdd383cc2fc7de75d080f8f18e6d10f
Ayoub Amer <ayouub.ameur@gmail.com>
2025-01-03 14:29:16 +0100

    feat: remove uneeded function

--- smart-contracts/assembly/lib/basicPoolMath.ts
+++ smart-contracts/assembly/lib/basicPoolMath.ts
@@ -2,28 +2,7 @@ import { u256 } from 'as-bignum/assembly';
 import { SafeMath256 } from './safeMath';
 import { generateEvent, print } from '@massalabs/massa-as-sdk';
 
-export const HUNDRED_PERCENT =u64(1000);
-
-export function getInputAmountNet(inputAmount: u256, feeRate: f64): u256 {
-  // Ensure feeRate is within a valid range [0, 1]
-  assert(feeRate >= 0.0 && feeRate <= 1.0, 'feeRate should be between 0 and 1');
-
-  print('inputAmount : ' + inputAmount.toString());
-  print('Fee rate : ' + feeRate.toString());
-  // Convert feeRate to a scaled integer (feeRate)
-  const feeRateScaled = f64ToU256(feeRate);
-
-  // Calculate the fee as: (inputAmount * feeRateScaled) / 100
-  const fee = SafeMath256.div(
-    SafeMath256.mul(inputAmount, feeRateScaled),
-    ONE_HUNDRED_PERCENT,
-  );
-
-  // Subtract the fee from the input amount
-  const inputAmountNet = SafeMath256.sub(inputAmount, fee);
-
-  return inputAmountNet;
-}
+export const HUNDRED_PERCENT = u64(1000);
 
 export function getFeeFromAmount(inputAmount: u256, feeRate: f64): u256 {
   const feeRateScaled = u256.fromF64(feeRate);
