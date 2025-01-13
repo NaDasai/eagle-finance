6889a47250a7e6523f331989c189d44b666b1cb4
Ayoub Amer <ayouub.ameur@gmail.com>
2025-01-13 19:12:22 +0100

    test: addLiq case when reserves are not empty

--- smart-contracts/assembly/__tests__/basicPool/addLiquidity.spec.ts
+++ smart-contracts/assembly/__tests__/basicPool/addLiquidity.spec.ts
@@ -17,6 +17,7 @@ import {
 import { Args, bytesToU256 } from '@massalabs/as-types';
 import { u256 } from 'as-bignum/assembly';
 import { parseMas } from '../utils';
+import { SafeMath256 } from '../../lib/safeMath';
 
 // addres of contract in @massalabs/massa-as-sdk/vm-mock/vm.js
 const contractAddr = 'AS12BqZEQ6sByhRLyEuf0YbQmcF2PsDdkNNG1akBJu9XcjZA1eT';
@@ -37,7 +38,7 @@ function switchUser(user: string): void {
   changeCallStack(user + ' , ' + contractAddr);
 }
 
-beforeAll(() => {
+function resetPoolContract(): void {
   resetStorage();
   addAddressToLedger(aTokenAddress);
   addAddressToLedger(bTokenAddress);
@@ -55,44 +56,110 @@ beforeAll(() => {
   mockScCall(new Args().add(user1Address).serialize());
 
   constructor(args.serialize());
-});
+}
 
-test('Test Case 1: Adding Liquidity while both Reserves are 0', () => {
-  switchUser(user2Address);
+beforeAll(() => resetPoolContract());
 
-  // Both Reserves should be 0 before adding liquidity
-  expect(bytesToU256(getLocalReserveA())).toStrictEqual(u256.Zero);
-  expect(bytesToU256(getLocalReserveB())).toStrictEqual(u256.Zero);
+describe('addLiquidity success cases', () => {
+  test('User 2 adds liquidity while both Reserves are 0', () => {
+    switchUser(user2Address);
 
-  const aAmount = parseMas(1);
-  const bAmount = parseMas(100);
-  const minAmountA = parseMas(0);
-  const minAmountB = parseMas(0);
+    // Both Reserves should be 0 before adding liquidity
+    expect(bytesToU256(getLocalReserveA())).toStrictEqual(u256.Zero);
+    expect(bytesToU256(getLocalReserveB())).toStrictEqual(u256.Zero);
 
-  print(`aAmount: ${aAmount.toString()}`);
-  print(`bAmount: ${bAmount.toString()}`);
+    const aAmount = parseMas(1);
+    const bAmount = parseMas(100);
+    const minAmountA = parseMas(0);
+    const minAmountB = parseMas(0);
 
-  const addLiquidityArgs = new Args()
-    .add(aAmount)
-    .add(bAmount)
-    .add(minAmountA)
-    .add(minAmountB)
-    .serialize();
+    print(`aAmount: ${aAmount.toString()}`);
+    print(`bAmount: ${bAmount.toString()}`);
 
-  // There is 2 transferFrom calls in addLiquidity which will be mocked
-  mockScCall(new Args().serialize());
-  mockScCall(new Args().serialize());
+    const addLiquidityArgs = new Args()
+      .add(aAmount)
+      .add(bAmount)
+      .add(minAmountA)
+      .add(minAmountB)
+      .serialize();
 
-  addLiquidity(addLiquidityArgs);
+    // There is 2 transferFrom calls in addLiquidity which will be mocked
+    mockScCall(new Args().serialize());
+    mockScCall(new Args().serialize());
 
-  // Both Reserves should be equal to the amount added
-  expect(bytesToU256(getLocalReserveA())).toStrictEqual(aAmount);
-  expect(bytesToU256(getLocalReserveB())).toStrictEqual(bAmount);
+    addLiquidity(addLiquidityArgs);
 
-  // LP balance should be equal to sqrt(aAmount * bAmount)
-  const expectedLPBalance = parseMas(10);
+    // Both Reserves should be equal to the amount added
+    const aRes = bytesToU256(getLocalReserveA());
+    const bRes = bytesToU256(getLocalReserveB());
 
-  expect(
-    bytesToU256(getLPBalance(new Args().add(user2Address).serialize())),
-  ).toStrictEqual(expectedLPBalance);
+    expect(aRes).toStrictEqual(aAmount);
+    expect(bRes).toStrictEqual(bAmount);
+
+    // LP balance should be equal to sqrt(aAmount * bAmount)
+    const expectedLPBalance = parseMas(10);
+
+    expect(
+      bytesToU256(getLPBalance(new Args().add(user2Address).serialize())),
+    ).toStrictEqual(expectedLPBalance);
+  });
+
+  test('User 3 adds liquidity while both Reserves are not 0', () => {
+    switchUser(user3Address);
+
+    const aRes = bytesToU256(getLocalReserveA());
+    const bRes = bytesToU256(getLocalReserveB());
+
+    // expect pool reserve to be equals to 1 and 100 MAS
+    expect(aRes).toStrictEqual(parseMas(1));
+    expect(bRes).toStrictEqual(parseMas(100));
+
+    // update initial k
+    const initialK = SafeMath256.mul(aRes, bRes);
+
+    print(`initialK: ${initialK.toString()}`);
+
+    // expect k to equal 100 * 1
+    expect(initialK).toStrictEqual(SafeMath256.mul(parseMas(1), parseMas(100)));
+
+    const aAmount = parseMas(2);
+    const bAmount = parseMas(200);
+    const minAmountA = parseMas(0);
+    const minAmountB = parseMas(0);
+
+    print(`aAmount: ${aAmount.toString()}`);
+    print(`bAmount: ${bAmount.toString()}`);
+
+    const addLiquidityArgs = new Args()
+      .add(aAmount)
+      .add(bAmount)
+      .add(minAmountA)
+      .add(minAmountB)
+      .serialize();
+
+    // There is 2 transferFrom calls in addLiquidity which will be mocked
+    mockScCall(new Args().serialize());
+    mockScCall(new Args().serialize());
+
+    addLiquidity(addLiquidityArgs);
+
+    const aResAfter = bytesToU256(getLocalReserveA());
+    const bResAfter = bytesToU256(getLocalReserveB());
+
+    print(`aResAfter: ${aResAfter.toString()}`);
+    print(`bResAfter: ${bResAfter.toString()}`);
+
+    // Both Reserves should be equal to the amount added
+    expect(aResAfter).toStrictEqual(SafeMath256.add(aAmount, aRes));
+    expect(bResAfter).toStrictEqual(SafeMath256.add(bAmount, bRes));
+
+    // LP balance should be equal to 20
+    const expectedLPBalance = parseMas(20);
+
+    print(`expectedLPBalance: ${expectedLPBalance.toString()}`);
+
+    expect(
+      bytesToU256(getLPBalance(new Args().add(user3Address).serialize())),
+    ).toStrictEqual(expectedLPBalance);
+  });
 });
