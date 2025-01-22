679d1512131601a05f223c40a73135b9dbf1ee5d
Ayoub Amer <ayouub.ameur@gmail.com>
2025-01-22 12:13:52 +0100

    test: new test scenario "addLiq with different token decimals"

--- smart-contracts/tests/basicPool.test.ts
+++ smart-contracts/tests/basicPool.test.ts
@@ -4,9 +4,11 @@ import {
   Account,
   Args,
   formatMas,
+  formatUnits,
   Mas,
   MRC20,
   parseMas,
+  parseUnits,
   SmartContract,
   Web3Provider,
 } from '@massalabs/massa-web3';
@@ -39,7 +41,7 @@ console.log('User1 address: ', user1Provider.address);
 console.log('User2 address: ', user2Provider.address);
 
 const wmasAddress = 'AS12FW5Rs5YN2zdpEnqwj4iHUUPt9R4Eqjq2qtpJFNKW3mn33RuLU';
-const aTokenAddress = 'AS1RWS5UNryey6Ue5HGLhMQk9q7YRnuS1u6M6JAjRwSfc2aRbZ5H';
+let aTokenAddress = 'AS1RWS5UNryey6Ue5HGLhMQk9q7YRnuS1u6M6JAjRwSfc2aRbZ5H';
 const bTokenAddress = wmasAddress;
 let poolFeeRate = 0;
 
@@ -1184,5 +1186,213 @@ describe('Scenario 3: Add liquidity, Swap, Remove liquidity with input fees', as
       lpAmountAfter,
       'User1 LP balance should be 0 after removing liquidity',
     ).toBe(parseMas('0'));
+  });
+});
+
+describe('Scenario 4: Add liquidity using different token decimals wmas(9 decimals) and token A (18 decimals)', async () => {
+  beforeAll(async () => {
+    // another token that its decimals are 18
+    aTokenAddress = 'AS12mmxJw7XF4iRpMpuMbYLcs2xX6EUKT9E9Bs7hzPPUxPk5ZZsaN';
+    poolFeeRate = 0.3 * 10_000;
+
+    registryContract = await deployRegistryContract(
+      user1Provider,
+      wmasAddress,
+      0.05,
+    );
+
+    // create new pool
+    await createNewPool(
+      registryContract,
+      aTokenAddress,
+      bTokenAddress,
+      poolFeeRate,
+    );
+
+    const pools = await getPools(registryContract);
+
+    console.log('Pools: ', pools);
+
+    expect(pools.length > 0, 'No pools found');
+
+    // get the last pool address
+    poolAddress = pools[pools.length - 1].poolAddress;
+
+    poolContract = new SmartContract(user1Provider, poolAddress);
+  });
+
+  test('User 1 Add liquidity to pool when its empty', async () => {
+    // get all pool reserves and expect them to be 0
+    const [reserveA, reserveB] = await getPoolReserves(poolContract);
+
+    expect(reserveA, 'Reserve should be 0 when pool is empty').toBe(0n);
+    expect(reserveB, 'Reserve should be 0 when pool is empty').toBe(0n);
+
+    const user1ATokenBalanceBefore = await getTokenBalance(
+      aTokenAddress,
+      user1Provider.address,
+      user1Provider,
+    );
+
+    const user1BTokenBalanceBefore = await getTokenBalance(
+      bTokenAddress,
+      user1Provider.address,
+      user1Provider,
+    );
+
+    console.log('User1 A Token balance before: ', user1ATokenBalanceBefore);
+    console.log('User1 B Token balance before: ', user1BTokenBalanceBefore);
+
+    const aAmount = 10;
+    const bAmount = 10;
+
+    // increase allowance of both tokerns amoutns first before adding liquidity
+    await increaseAllownace(
+      aTokenAddress,
+      poolAddress,
+      aAmount,
+      user1Provider,
+      18,
+    );
+    await increaseAllownace(bTokenAddress, poolAddress, bAmount, user1Provider);
+
+    // add liquidity
+    await addLiquidity(poolContract, aAmount, bAmount, 0, 0, 18);
+
+    // get teh reserves
+    const [reserveAAfter, reserveBAfter] = await getPoolReserves(poolContract);
+
+    console.log('Reserve A after: ', reserveAAfter);
+    console.log('Reserve B after: ', reserveBAfter);
+
+    const user1ATokenBalanceAfter = await getTokenBalance(
+      aTokenAddress,
+      user1Provider.address,
+      user1Provider,
+    );
+
+    const user1BTokenBalanceAfter = await getTokenBalance(
+      bTokenAddress,
+      user1Provider.address,
+      user1Provider,
+    );
+
+    console.log('User1 A Token balance after: ', user1ATokenBalanceAfter);
+    console.log('User1 B Token balance after: ', user1BTokenBalanceAfter);
+
+    expect(
+      user1ATokenBalanceAfter,
+      'User1 A Token balance should decrease after adding liquidity',
+    ).toBeLessThan(user1ATokenBalanceBefore);
+
+    expect(
+      user1BTokenBalanceAfter,
+      'User1 B Token balance should decrease after adding liquidity',
+    ).toBeLessThan(user1BTokenBalanceBefore);
+
+    expect(reserveAAfter, 'Reserve A should be 10 after adding liquidity').toBe(
+      parseUnits('10', 18),
+    );
+
+    expect(reserveBAfter, 'Reserve B should be 10 after adding liquidity').toBe(
+      parseMas('10'),
+    );
+
+    // get the lp balance of user1
+    const user1LPBalance = await getLPBalance(
+      poolContract,
+      user1Provider.address,
+    );
+
+    console.log('User1 LP balance: ', user1LPBalance);
+
+    const m = 10000000000n * 10000000000000000000n;
+    const sqrtM = Math.sqrt(Number(m));
+
+    expect(user1LPBalance, 'User1 LP balance should be 10').toBe(
+      BigInt(sqrtM.toString().split('.')[0]),
+    );
+  });
+
+  test("User 2 adds liquidity to pool's reserves", async () => {
+    // switch poolContrcat to user2 provider
+    poolContract = new SmartContract(user2Provider, poolAddress);
+
+    const [reserveA, reserveB] = await getPoolReserves(poolContract);
+
+    expect(reserveA, 'Reserve should be 0 when pool is empty').toBe(
+      parseUnits('10', 18),
+    );
+    expect(reserveB, 'Reserve should be 0 when pool is empty').toBe(
+      parseUnits('10', 9),
+    );
+
+    const user2ATokenBalanceBefore = await getTokenBalance(
+      aTokenAddress,
+      user2Provider.address,
+      user2Provider,
+    );
+
+    const user2BTokenBalanceBefore = await getTokenBalance(
+      bTokenAddress,
+      user2Provider.address,
+      user2Provider,
+    );
+
+    const aAmount = 10;
+    const bAmount = 10;
+
+    // check if user 2 has enough tokens to add liquidity
+    expect(
+      user2ATokenBalanceBefore,
+      'User2 does not have enough A tokens to add liquidity',
+    ).toBeGreaterThanOrEqual(parseUnits(aAmount.toString(), 18));
+
+    expect(
+      user2BTokenBalanceBefore,
+      'User2 does not have enough B tokens to add liquidity',
+    ).toBeGreaterThanOrEqual(parseUnits(bAmount.toString(), 9));
+
+    // increase allowance of both tokerns amoutns first before adding liquidity
+    await increaseAllownace(
+      aTokenAddress,
+      poolAddress,
+      aAmount,
+      user2Provider,
+      18,
+    );
+
+    await increaseAllownace(bTokenAddress, poolAddress, bAmount, user2Provider);
+
+    // add liquidity
+    await addLiquidity(poolContract, aAmount, bAmount, 0, 0, 18);
+
+    // get the reserves after adding liquidity
+    const [reserveAAfter, reserveBAfter] = await getPoolReserves(poolContract);
+
+    console.log('Reserve A after: ', reserveAAfter);
+    console.log('Reserve B after: ', reserveBAfter);
+
+    expect(reserveAAfter, 'Reserve A should be 20 after adding liquidity').toBe(
+      parseUnits('20', 18),
+    );
+    expect(reserveBAfter, 'Reserve B should be 20 after adding liquidity').toBe(
+      parseUnits('20', 9),
+    );
+
+    // get the lp balance of user2
+    const user2LPBalance = await getLPBalance(
+      poolContract,
+      user2Provider.address,
+    );
+
+    console.log('User2 LP balance: ', user2LPBalance);
+
+    const m = 10000000000n * 10000000000000000000n;
+    const sqrtM = Math.sqrt(Number(m));
+
+    expect(user2LPBalance, 'User2 LP balance should be 10').toBe(
+      BigInt(sqrtM.toString().split('.')[0]),
+    );
   });
 });
