1713b913344d08e7c784ef6d0b36af6a1b8d7e2e
Ayoub Amer <ayouub.ameur@gmail.com>
2025-01-27 17:30:24 +0100

    feat: add deploy flashSwap call

---
+++ smart-contracts/tests/calls/flashSwap.ts
@@ -0,0 +1,28 @@
+import { Args, Mas, Provider, SmartContract } from '@massalabs/massa-web3';
+import { getScByteCode } from '../utils';
+
+export async function deployFlashSwapContract(
+  userProvider: Provider,
+  poolAddress: string,
+  registryAddress: string,
+) {
+  const flashSwapByteCode = getScByteCode('build', 'ExempleFlashSwap.wasm');
+
+  const constructorArgs = new Args()
+    .addString(poolAddress)
+    .addString(registryAddress)
+    .serialize();
+
+  const contract = await SmartContract.deploy(
+    userProvider,
+    flashSwapByteCode,
+    constructorArgs,
+    {
+      coins: Mas.fromString('2'),
+    },
+  );
+
+  console.log('FlashSwap contract deployed at:', contract.address);
+
+  return contract;
+}
