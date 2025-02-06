5fdaf57f62a57328f4b2dd0200c360fdc8164eef
Ayoub Amer <ayouub.ameur@gmail.com>
2025-02-06 11:01:19 +0100

    fix: modify create new token event

--- smart-contracts/assembly/contracts/tokenDeployer.ts
+++ smart-contracts/assembly/contracts/tokenDeployer.ts
@@ -103,16 +103,7 @@ export function createNewToken(binaryArgs: StaticArray<u8>): void {
 
   // Emit an event
   generateEvent(
-    createEvent('CREATE_NEW_TOKEN', [
-      tokenName,
-      tokenSymbol,
-      decimals.toString(),
-      totalSupply.toString(),
-      url,
-      description,
-      tokenAddress.toString(),
-      coinsToUseOnDeploy.toString(),
-    ]),
+    `CREATE_NEW_TOKEN:${tokenName}||${tokenSymbol}||${decimals.toString()}||${totalSupply.toString()}||${url}||${description}||${tokenAddress.toString()}||${coinsToUseOnDeploy.toString()}`,
   );
 
   // Raw event to be able to get the token address at the frotnend by using operation.getDeployedAddress(true)
